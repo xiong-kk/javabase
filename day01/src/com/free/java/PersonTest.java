5e79fa6887bc0315cd1ca53c33bd9f65c66a71cb
free <hunter.com>
2024-10-26 21:35:58 +0800

    复习基础

--- day01/src/com/free/java/PersonTest.java
+++ day01/src/com/free/java/PersonTest.java
@@ -5,4 +5,25 @@ package com.free.java;
  * @create 2024-10-26-21:14
  */
 public class PersonTest {
+    public static void main(String[] args) {
+        Person person=new Person();
+        person.eat();
+        person.sleep();
+        person.talk("中文");
+    }
+}
+class Person{
+    String name;
+    int age=1;
+    boolean isMale;
+
+    public void eat(){
+        System.out.println("人吃饭");
+    }
+    public void sleep(){
+        System.out.println("人睡觉");
+    }
+    public void talk(String language){
+        System.out.println("人说话，语言是"+language);
+    }
 }
