9253b51e028ad642d10ad53fd63ce02ef78d79f4
free <hunter.com>
2025-08-25 14:28:36 +0800

    复习基础

---
+++ day00/src/com/free/java/VariableTest.java
@@ -0,0 +1,9 @@
+package com.free.java;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte b1=2;
+        int i1=12;
+        //byte b2=b1+i1;编译不通过，运算结果会转换为一个int类型
+    }
+}
